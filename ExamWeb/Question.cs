f3b2d1899576d07b6ee25eba27ee744af55c131f
Wiktor Kowalski <wiktor9196667@gmail.com>
2021-02-01 00:40:17 +0100

    Add project files.

---
+++ ExamWeb/Question.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExamWeb
+{
+    public class Question
+    {
+        public string subject { get; set; }
+        public string question { get; set; }
+        public IEnumerable<Answer> answers { get; set; }
+    }
+
+    public class Answer
+    {
+        public string author { get; set; }
+        public string answer { get; set; }
+    }
+}
